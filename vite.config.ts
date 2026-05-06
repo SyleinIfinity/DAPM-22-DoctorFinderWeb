6d7101e676bef75610fa887a9443914579abb1a4
Syleinfinity <khanhsky2k5nam@gmail.com>
2026-05-06 16:44:41 +0700

    fix loi chiem port

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    port: 5173,
+    strictPort: true,
     proxy: {
       '/finder-doctor': {
         target: 'https://34.126.165.66',
